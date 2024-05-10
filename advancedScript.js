cd0bde01397f865fcae1e60db83e0f4a3bc292ee
kabindrr <chaina_kabindra@yahoo.com>
2024-05-11 09:35:51 +1000

    Class OOP and This

--- advancedScript.js
+++ advancedScript.js
@@ -51,17 +51,40 @@ console.log("Advanced Scripting Concepts");
 
 //Factiory Function Methods
 
-function aboutPerson(name, add) {
-  return {
-    name,
-    add,
-    bio() {
-      return `My name is ${this.name} and ${this.name} lives in ${this.add} `;
-    },
-  };
-}
-const kabiBio = aboutPerson("Kabindra", "Sydney");
-console.log(kabiBio.bio());
+// function aboutPerson(name, add) {
+//   return {
+//     name,
+//     add,
+//     bio() {
+//       return `My name is ${this.name} and ${this.name} lives in ${this.add} `;
+//     },
+//   };
+// }
+// const kabiBio = aboutPerson("Kabindra", "Sydney");
+// console.log(kabiBio.bio());
 
-const premBio = aboutPerson("Prem", "Uk");
-console.log(premBio.bio());
+// const premBio = aboutPerson("Prem", "Uk");
+// console.log(premBio.bio());
+
+// Constructor Function
+
+// function AboutPerson(name, add) {
+//   this.name = name;
+//   this.add = add;
+// }
+
+// AboutPerson.prototype.bio = function () {
+//   return `My name is ${this.name} and ${this.name} lives in ${this.add} `;
+// };
+// const kabiBio = new AboutPerson("Kabindra", "Sydney");
+// console.log(kabiBio.bio());
+
+//// Class Based OOP
+
+// class Person {}
+
+// This KeyWord Use Cases
+// Global Context
+
+console.log(window, this);
+console.log(window === this);
